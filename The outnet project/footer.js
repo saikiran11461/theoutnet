fddf1b737eab23a3668e2895496998f23b9d32bf
saikiran <saikiran11461@gmail.com>
2022-03-31 11:54:19 +0530

    first commit

---
+++ The outnet project/footer.js
@@ -0,0 +1,57 @@
+function footer() {
+    return `  <div class="footer">
+    <!-- <div class="footadd"> </div> -->
+    <div class="com">
+        <h4>Help and Information</h4>
+        <p>About Us</p>
+        <p>Affiliates</p>
+        <p>Careers</p>
+        <p>Contact Us</p>
+        <p>Help</p>
+        <p>Clothing Care Guides</p>
+        <p>How To Find Your Size With Fit Finder</p>
+        <p>Privacy Policy</p>
+        <p>Terms and Conditions</p>
+        <div class="visa"><img src="https://i.ibb.co/pfjk2KQ/Screenshot-2022-03-26-at-11-04-44-AM.png" alt=""></div>
+    </div>
+    <div class="exp">
+        <p>Our Considered Pledge</p>
+        <p>Payment</p>
+        <p>Delivery</p>
+        <p> Returns & Refunds</p>
+        <p>Track your Order</p>
+        <p>Create a Return</p>
+        <p>Track your Return</p>
+        <p>Cookie Policy</p>
+    </div>
+    <div class="pol">
+        <h4>Want to stay stylishly in the know?</h4>
+        <p>Sign up today for email exclusives including early access to Sale, the latest trends and arrivals from your favorite designers and can’t miss promotions.</p>
+     <div class="email_input"> 
+         <div><p>Enter Your Email Address</p></div>
+         <div><p>Subcribe</p></div>
+     </div>
+     <h4>24/7 Customer Care</h4>
+     <div class="small_icon"><img src="https://i.ibb.co/4V5mK05/Screenshot-2022-03-26-at-10-38-33-AM.png" alt="image">
+        <div><p>+44 330 022 4250</p></div>
+   </div> 
+   <div class="small_icon"><img src="https://i.ibb.co/rmWLXzf/Screenshot-2022-03-26-at-10-42-06-AM.png" alt="">
+   <div><p>customercare@theoutnet.com</p></div>
+   </div>
+   <div class="social_media"><img src="https://i.ibb.co/jRzb8zg/Screenshot-2022-03-26-at-10-38-27-AM.png" alt=""></div>
+    </div>
+  
+</div>
+<hr/>
+<div class="end">
+    <div><h4>NET-A-PORTER</h4></div>
+    <div><h4>MR PORTER</h4></div>
+    <div><p>© 2009 - 2022 THE OUTNET , part of YOOX NET-A-PORTER GROUP.</p>
+       <p> The individuals featured on this site do not endorse THE OUTNET or the products shown.</p></div>
+</div>
+
+</div>`
+}
+
+
+export default footer
---
+++ The outnet project/header.js
@@ -0,0 +1,88 @@
+function header() {
+    return `<div id="header">
+    <div id="first">
+        <a href="#">
+            <h3>Track your Order </h3>
+        </a>
+        <a href="#">
+            <h3>Create A Return</h3>
+        </a>
+        <a href="#">
+            <h3> Customer Care</h3>
+        </a>
+        <a href="#">
+            <h3>Delivery</h3>
+        </a>
+        <a href="#">
+            <h3>Returns</h3>
+        </a>
+    </div>
+
+
+
+    <div id="second">
+        <div>
+            <a href="register.html">
+
+                <h3 class="border"><i class="fa-solid fa-user"></i>Sign in</h3>
+            </a>
+            <a href="#">
+                <h3> <i class="fa-indian-rupee-sign"></i> India</h3>
+            </a>
+
+            <h1>THE OUTNET</h1>
+
+            <a href="#">
+                <h3 class="border"><i class="fa-solid fa-magnifying-glass"></i> Search</h3>
+            </a>
+            <a href="cart.html">
+                <h3><i class="fa-solid fa-heart"></i>cart </h3>
+            </a>
+            <a href="#">
+                <h3> <i class="fa-solid fa-cart-arrow-down"></i></h3>
+            </a>
+        </div>
+        <div class="seconddiv">
+            <a href="product.html">
+                <h3 class="space">Superbrands</h3>
+            </a>
+            <a href="#">
+                <h3>Just in</h3>
+            </a>
+            <a href="#">
+                <h3>Bestsellers</h3>
+            </a>
+            <a href="#">
+                <h3>Designs</h3>
+            </a>
+            <a href="#">
+                <h3>Clothing</h3>
+            </a>
+            <a href="#">
+                <h3>Shoes</h3>
+            </a>
+            <a href="#">
+                <h3>Bags</h3>
+            </a>
+            <a href="#">
+                <h3>Accessiores</h3>
+            </a>
+            <a href="#">
+                <h3>Editors</h3>
+            </a>
+            <a href="#">
+                <h3>Men's</h3>
+            </a>
+        </div>
+        <hr>
+    </div>
+
+    <div id="third">
+        <div><i class="fa-solid fa-van-shuttle"></i>From Balmain to Buberry,discover our list of must-have Superbrands</div>
+        <div id="gap"><i class="fa-solid fa-clothes-hanger"></i> 15% off your first clothing order with FIRST15</div>
+        <div><i class="fa-solid fa-mobile-screen"></i> App-y shopping:download our new app</div>
+    </div>
+</div>`
+}
+
+export default header
